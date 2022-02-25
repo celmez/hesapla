2a8fc025791328b722dc47dd721b0ce58493b1a8
Yusuf Çelmez <72373760+celmez@users.noreply.github.com>
2022-02-25 22:37:16 +0300

    Update and rename main.js to calculate.js

---
+++ calculate.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('#submit').click(function()
+    {
+        var vize = $('#vize').val()
+        var final = $('#final').val()
+
+        var a = vize * 40 / 100
+        var b = final * 60 / 100
+        var toplam = a + b
+
+        if( vize == "" )
+        {
+            alert("Ders ortalamanızın hesaplanabilmesi için lütfen vize notunuzu giriniz")
+        }
+
+        else if( final == "" )
+        {
+            alert("Ders ortalamanızın hesaplanabilmesi için lütfen final notunuzu giriniz")
+        }
+
+        else if( toplam == 60 )
+        {
+            alert("Tebrikler "+toplam+" ortalama ile bu dersten geçtiniz...")
+        }
+
+        else if( toplam > 60 )
+        {
+            alert("Tebrikler "+toplam+" ortalama ile bu dersten geçtiniz...")
+        }
+
+        else if( final < "50" )
+        {
+            alert("Maalesef ortalamanız 50'nin altında olduğunuz için dersten otomatik olarak kaldınız! Ortalamanız: "+toplam)
+        }
+
+        else
+        {
+            alert("Maalesef "+toplam+" ortalama ile bu dersten kaldınız!!!")
+        }
+    })
+})
